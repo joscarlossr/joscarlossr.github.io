fd6a9f21676ff454816a2d7747ce43414ece3306
Carlos Silva <joscarlossr@gmail.com>
2020-06-03 23:04:27 -0300

    Arquivos para a página

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// Get the modal
+var modal = document.getElementById("particlesModal");
+modal.style.display = "block";
+
+$('#particlesModal').particlesModal({
+	invertX: true,
+	invertY: true,
+	scalarX: 10,
+	 frictionY: .1
+});
